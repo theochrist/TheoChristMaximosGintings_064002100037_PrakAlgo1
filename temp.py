8a3a87e3321713242ca98c38c976c8abb8884fee
theochrist <91509499+theochrist@users.noreply.github.com>
2021-09-27 21:46:03 -0700

    init

---
+++ temp.py
@@ -0,0 +1,7 @@
+#Mengubah huruf dengan menggunakan perintah upper()
+print ("Mengubah Huruf Kecil menjadi Kapital")
+print("silahkan masukan huruf yang ingin diubah")
+a=input()
+#perintah input() untuk memasukan string
+print("Menjadi= ", a.upper())
+print("===========================================")
